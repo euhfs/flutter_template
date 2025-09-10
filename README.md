37fd47ca6b8f6032dea3af51c51d9cc787469bf8
euhfs <euhfs2@gmail.com>
2025-09-10 18:23:56 +0100

    readme + build gradle

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 # Instalation
 
-- git clone https://github.com/euhfs/flutter_template.git
-- cd flutter_template
-- code .
+- Download the .zip from the Code button
+
+# IMPORTANT
+
+To add other platforms run the command for each platform inside the root folder you want to add for your project:
+
+- flutter create . --platforms=windows
+- flutter create . --platforms=linux
+- flutter create . --platforms=web
+- flutter create . --platforms=macos
+- flutter create . --platforms=ios
+
+
 
 # Predefinied dark/light theme + custom colors
 
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,21 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// Load keystore properties
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
+    // TODO: change this to your application's package name
     namespace = "com.example.flutter_template"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -20,16 +30,33 @@ android {
     }
 
     defaultConfig {
+        // TODO: change this to your application's package name
         applicationId = "com.example.flutter_template"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+
+        // Change to your desired values. Recommended: 
+        minSdkVersion = 23
+        targetSdkVersion = 35
+
+        // Leave these values like this, to change version change it in pubspec.yaml which changed the version globally for each platform
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it.toString()) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+
     buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
