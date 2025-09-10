7188087ee8e04b12e3e2987e66e9d05d4fcf4314
euhfs <euhfs2@gmail.com>
2025-09-10 17:38:40 +0100

    more text styles

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -12,8 +12,81 @@ class AppTheme {
       ),
       iconTheme: const IconThemeData(color: AppColors.lightIconColor),
       textTheme: const TextTheme(
-        bodyLarge: TextStyle(color: AppColors.lightPrimaryText),
-        bodyMedium: TextStyle(color: AppColors.lightSecondaryText),
+        displayLarge: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 57,
+          fontWeight: FontWeight.bold,
+        ),
+        displayMedium: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 45,
+          fontWeight: FontWeight.bold,
+        ),
+        displaySmall: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 36,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineLarge: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 32,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineMedium: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 28,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineSmall: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+        ),
+        titleLarge: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 22,
+          fontWeight: FontWeight.w500,
+        ),
+        titleMedium: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+        titleSmall: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyLarge: TextStyle(
+          color: AppColors.lightPrimaryText,
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+        bodyMedium: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+        ),
+        bodySmall: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 12,
+          fontWeight: FontWeight.normal,
+        ),
+        labelLarge: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        labelMedium: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+        labelSmall: TextStyle(
+          color: AppColors.lightSecondaryText,
+          fontSize: 11,
+          fontWeight: FontWeight.w500,
+        ),
       ),
       textSelectionTheme: const TextSelectionThemeData(
         cursorColor: Colors.blue,
@@ -35,8 +108,81 @@ class AppTheme {
       ),
       iconTheme: const IconThemeData(color: AppColors.darkIconColor),
       textTheme: const TextTheme(
-        bodyLarge: TextStyle(color: AppColors.darkPrimaryText),
-        bodyMedium: TextStyle(color: AppColors.darkSecondaryText),
+        displayLarge: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 57,
+          fontWeight: FontWeight.bold,
+        ),
+        displayMedium: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 45,
+          fontWeight: FontWeight.bold,
+        ),
+        displaySmall: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 36,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineLarge: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 32,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineMedium: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 28,
+          fontWeight: FontWeight.w600,
+        ),
+        headlineSmall: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+        ),
+        titleLarge: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 22,
+          fontWeight: FontWeight.w500,
+        ),
+        titleMedium: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+        titleSmall: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyLarge: TextStyle(
+          color: AppColors.darkPrimaryText,
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+        bodyMedium: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+        ),
+        bodySmall: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 12,
+          fontWeight: FontWeight.normal,
+        ),
+        labelLarge: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        labelMedium: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+        ),
+        labelSmall: TextStyle(
+          color: AppColors.darkSecondaryText,
+          fontSize: 11,
+          fontWeight: FontWeight.w500,
+        ),
       ),
       textSelectionTheme: const TextSelectionThemeData(
         cursorColor: Colors.blue,
