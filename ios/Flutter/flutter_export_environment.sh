02768dac08c86e80c20d9b036a0345b3294a15b9
euhfs <euhfs2@gmail.com>
2026-01-02 22:18:49 +0000

    s

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/user/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/media/veracrypt1/code/flutter/cross_platform/tasks"
+export "FLUTTER_APPLICATION_PATH=/media/veracrypt1/code/flutter/flutter_template"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
